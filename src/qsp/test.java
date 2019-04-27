57eedde9305c859155c306a186f8d01d8d10fb87
Monisha <Monisha@DESKTOP-LLTFM9O>
2019-04-27 01:32:12 -0700

    new commit

---
+++ src/qsp/test.java
@@ -0,0 +1,17 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test 
+{
+	
+		@Test
+	 public void testA()
+	 {
+		 Reporter.log("TestA..",true);
+	 }
+	
+
+
+}
